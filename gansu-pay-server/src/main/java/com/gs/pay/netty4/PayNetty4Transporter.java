7222be1922e702c42894032654beb10010497f5f
zhangxiaoping <zhangxiaoping91@163.com>
2018-01-16 21:52:32 +0800

    netty4接入

---
+++ gansu-pay-server/src/main/java/com/gs/pay/netty4/PayNetty4Transporter.java
@@ -0,0 +1,22 @@
+package com.gs.pay.netty4;
+
+import io.netty.channel.ChannelHandler;
+
+import java.net.URL;
+
+/**
+ * @author ：前世小和尚
+ * @Description ：netty4
+ * @ClassName ：PayNetty4Transporter
+ * @date ：2018/1/16 21:00:00
+ */
+public class PayNetty4Transporter {
+
+    public void serverBind(URL url, ChannelHandler listener) throws Exception{
+
+    }
+
+    public void clientConnect(URL url,ChannelHandler listener) throws Exception{
+
+    }
+}
---
+++ gansu-pay-server/src/main/java/com/gs/pay/netty4/server/PayNetty4Server.java
@@ -0,0 +1,18 @@
+package com.gs.pay.netty4.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ：前世小和尚
+ * @Description ：netty4服务端
+ * @ClassName ：PayNetty4Server
+ * @date ：2018/1/16 21:49:49
+ */
+public class PayNetty4Server {
+    private static final Logger log = LoggerFactory.getLogger(PayNetty4Server.class);
+
+    public void bind() throws Exception{
+
+    }
+}
